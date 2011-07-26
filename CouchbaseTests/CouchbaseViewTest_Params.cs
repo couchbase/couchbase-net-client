e75b8c9bf63256611f288eebe14294f11779f21d
Attila Kiskó <a@enyim.com>
2011-07-26 21:49:43 +0200

    Split the view tests into multiple files.
    Change-Id: I45884eb2cb19f811de8948b0d5f138e0f7033e3f
    Reviewed-on: http://review.couchbase.org/8383
    Reviewed-by: Matt Ingenthron <matt@couchbase.com>
    Tested-by: Matt Ingenthron <matt@couchbase.com>

---
+++ CouchbaseTests/CouchbaseViewTest_Params.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Couchbase;
+using System.IO;
+using Enyim.Caching;
+using FluentAssertions;
+using Moq;
+using Couchbase.Configuration;
+
+namespace CouchbaseTests
+{
+	public partial class CouchbaseViewTest
+	{
+		private IHttpClientLocator CreateParameterValidatingLocator(Dictionary<string, string> collectedParameters, List<string> requestedPaths)
+		{
+			var response = new Mock<IHttpResponse>();
+			response.Setup(r => r.GetResponseStream()).Returns(Stream.Null);
+
+			var request = new Mock<IHttpRequest>();
+			request.SetupAllProperties();
+
+			request.Setup(r => r.GetResponse()).Returns(response.Object);
+			request.Setup(r => r.AddParameter(It.IsAny<string>(), It.IsAny<string>())).
+					Callback<string, string>((k, v) => collectedParameters.Add(k, v));
+
+			var client = new Mock<IHttpClient>();
+			client.Setup(c => c.CreateRequest(It.IsAny<string>())).
+					Returns<string>(url =>
+					{
+						// log the url
+						requestedPaths.Add(url);
+
+						return request.Object;
+					});
+
+			var locator = new Mock<IHttpClientLocator>();
+			locator.Setup(l => l.Locate(It.IsAny<string>())).Returns(client.Object);
+
+			return locator.Object;
+		}
+
+		private void CheckViewParameters(Func<IView, IView> setupView, Dictionary<string, string> expectedParameters)
+		{
+			var collectedParameters = new Dictionary<string, string>();
+			var requestedPaths = new List<string>();
+			var locator = CreateParameterValidatingLocator(collectedParameters, requestedPaths);
+
+			IView view = new CouchbaseView(new Mock<IMemcachedClient>().Object, locator, "doc", "index");
+
+			setupView(view).ToList();
+
+			collectedParameters.Should().Equal(expectedParameters);
+		}
+
+		[TestMethod]
+		public void KeyRangeParametersShouldBeApplied()
+		{
+			CheckViewParameters(
+				view => view.StartKey("start-key").EndKey("end-key"),
+				new Dictionary<string, string>()
+				{
+					{ "startKey", "start-key" },
+					{ "endKey", "end-key" }
+				});
+		}
+
+		[TestMethod]
+		public void DocumentIdRangeParametersShouldBeApplied()
+		{
+			CheckViewParameters(
+				view => view.StartDocumentId("start-doc").EndDocumentId("end-doc"),
+				new Dictionary<string, string>()
+				{
+					{ "startKey_docid", "start-doc" },
+					{ "endKey_docid", "end-doc" }
+				});
+		}
+
+		[TestMethod]
+		public void DocumentIdRangeShouldRemoveKeyRange()
+		{
+			CheckViewParameters(
+				view => view.
+							StartKey("start-key").
+							EndKey("end-key").
+							StartDocumentId("start-doc").
+							EndDocumentId("end-doc"),
+				new Dictionary<string, string>()
+				{
+					{ "startKey_docid", "start-doc" },
+					{ "endKey_docid", "end-doc" }
+				});
+		}
+
+		[TestMethod]
+		public void KeyRangeShouldRemoveDocumentIdRange()
+		{
+			CheckViewParameters(
+				view => view.
+							StartDocumentId("start-doc").
+							EndDocumentId("end-doc").
+							StartKey("start-key").
+							EndKey("end-key"),
+				new Dictionary<string, string>()
+				{
+					{ "startKey", "start-key" },
+					{ "endKey", "end-key" }
+				});
+		}
+
+		[TestMethod]
+		public void LimitParameterShouldBeApplied()
+		{
+			CheckViewParameters(
+				view => view.Limit(1234),
+				new Dictionary<string, string>()
+				{
+					{ "limit", "1234" }
+				});
+		}
+
+		[TestMethod]
+		public void SkipParameterShouldBeApplied()
+		{
+			CheckViewParameters(
+				view => view.Skip(4567),
+				new Dictionary<string, string>()
+				{
+					{ "skip", "4567" }
+				});
+		}
+
+		[TestMethod]
+		public void StaleParameterShouldBeApplied()
+		{
+			CheckViewParameters(
+				view => view.Stale(StaleMode.AllowStale),
+				new Dictionary<string, string>()
+				{
+					{ "stale", "ok" }
+				});
+
+			CheckViewParameters(
+				view => view.Stale(StaleMode.UpdateAfter),
+				new Dictionary<string, string>()
+				{
+					{ "stale", "update_after" }
+				});
+		}
+
+		[TestMethod]
+		public void ReduceParameterShouldBeApplied()
+		{
+			CheckViewParameters(
+				view => view.Reduce(false),
+				new Dictionary<string, string>()
+				{
+					{ "reduce", "false" }
+				});
+
+			CheckViewParameters(
+			view => view.Reduce(true),
+			new Dictionary<string, string>()
+				{
+					{ "reduce", "true" }
+				});
+		}
+
+		[TestMethod]
+		public void GroupParameterShouldBeApplied()
+		{
+			CheckViewParameters(
+				view => view.Group(false),
+				new Dictionary<string, string>()
+				{
+					{ "group", "false" }
+				});
+
+			CheckViewParameters(
+			view => view.Group(true),
+			new Dictionary<string, string>()
+				{
+					{ "group", "true" }
+				});
+		}
+
+		[TestMethod]
+		public void GroupAtParameterShouldBeApplied()
+		{
+			CheckViewParameters(
+				view => view.GroupAt(4567),
+				new Dictionary<string, string>()
+				{
+					{ "group_level", "4567" }
+				});
+		}
+
+		[TestMethod]
+		public void InclusiveParameterShouldBeApplied()
+		{
+			CheckViewParameters(
+				view => view.WithInclusiveEnd(true),
+				new Dictionary<string, string>()
+				{
+					{ "inclusive_end", "true" }
+				});
+
+			CheckViewParameters(
+			view => view.WithInclusiveEnd(false),
+			new Dictionary<string, string>()
+				{
+					{ "inclusive_end", "false" }
+				});
+		}
+
+		[TestMethod]
+		public void DescendingParameterShouldBeApplied()
+		{
+			CheckViewParameters(
+				view => view.Descending(true),
+				new Dictionary<string, string>()
+				{
+					{ "descending", "true" }
+				});
+
+			CheckViewParameters(
+			view => view.Descending(false),
+			new Dictionary<string, string>()
+				{
+					{ "descending", "false" }
+				});
+		}
+
+		[TestMethod]
+		public void NoParameterShouldBeApplied()
+		{
+			CheckViewParameters(
+				view => view,
+				new Dictionary<string, string>() { });
+		}
+	}
+}
