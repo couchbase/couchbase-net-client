302c6f5bd1f08f75bb431ba1aa2f75975fc0da42
jeffrymorris <jeffrymorris@gmail.com>
2013-11-11 11:59:10 -0800

    NCBC-318: Status Code list contains duplicate value
    Change-Id: I5cdaaa1311c768dc128749a9ea561351faa481b9
    Reviewed-on: http://review.couchbase.org/30237
    Reviewed-by: Jeffry Morris <jeffrymorris@gmail.com>
    Tested-by: Jeffry Morris <jeffrymorris@gmail.com>

--- src/Enyim.Caching/StatusCode.cs
+++ src/Enyim.Caching/StatusCode.cs
@@ -2,8 +2,8 @@
 {
     public enum StatusCode
     {
-       /*Server Side StatusCodes: http://code.google.com/p/memcached/wiki/BinaryProtocolRevamped#Response_Status */
-        NoError = 0x0000,
+        /*Server Side StatusCodes: https://github.com/couchbase/libcouchbase/blob/bb1a3edfd89aa0c1cc3e0417db5341706aaa117e/include/memcached/protocol_binary.h*/
+        Success = 0x0000,
         KeyNotFound = 0x0001,
         KeyExists = 0x0002,
         ValueTooLarge = 0x0003,
@@ -11,9 +11,10 @@
         ItemNotStored = 0x0005,
         IncrDecrOnNonNumericValue = 0x0006,
         VBucketBelongsToAnotherServer = 0x0007,
-        AuthenticationError = 0x0008,
-        AuthenticationContinue = 0x0009,
-        UnknownCommand = 0x0082,
+        AuthenticationError = 0x0020,
+        AuthenticationContinue = 0x0021,
+        InvalidRange = 0x0022,
+        UnknownCommand = 0x0081,
         OutOfMemory = 0x0082,
         NotSupported = 0x0083,
         InternalError = 0x0084,
