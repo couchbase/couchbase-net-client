3f2354f2a0034c79fa31a1d517872de4d8248985
sdir456 <sdir456@gmail.com>
2014-01-29 14:11:17 +0100

    NCBC-375: preferring ipv4-address
    If you configure a couchbase-server with hostname instead of ip (ipv4, you
    can't enter an ipv6 address via web interface), the .net client library
    refuses the connection. This fix ensures that the ipv4-address is chosen
    over the ipv6-address when a connection is made.
    
    Additional info: MB-6378
    
    Change-Id: I0278d332ce16ac42158420fb147b4a00fae2e2dd
    Reviewed-on: http://review.couchbase.org/33084
    Reviewed-by: Jeffry Morris <jeffrymorris@gmail.com>
    Reviewed-by: Stefan Dirks <sdirks456@gmail.com>
    Tested-by: Jeffry Morris <jeffrymorris@gmail.com>

--- src/Couchbase/CouchbasePool.cs
+++ src/Couchbase/CouchbasePool.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Threading;
 using Couchbase.Management;
 using Enyim.Caching.Configuration;
@@ -296,11 +297,10 @@ namespace Couchbase
 			if (items.Length > 0)
 			{
 				if (log.IsDebugEnabled)
-					foreach (IPAddress item in items)
-						log.DebugFormat("Found address {0} for {1}", item, hostname);
+				    foreach (IPAddress item in items)
+				        log.DebugFormat("Found address {0} for {1}", item, hostname);
 
-				var retval = items[0];
-
+			    var retval = items.FirstOrDefault(item => item.AddressFamily == AddressFamily.InterNetwork) ?? items[0];
 				if (log.IsDebugEnabled)
 					log.DebugFormat("Using address {0} for {1}", retval, hostname);
 
