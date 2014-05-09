e70cd0113c5eb63bac8b8ce0048ffebc526d4e70
jeffrymorris <jeffrymorris@gmail.com>
2014-05-09 13:54:38 -0700

    NCBC-471: Add support for Remove (delete) operation

---
+++ Src/Couchbase/IO/Operations/DeleteOperation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Couchbase.Core;
+
+namespace Couchbase.IO.Operations
+{
+    /// <summary>
+    /// Removes a key from the database, failing if it doesn't exist.
+    /// </summary>
+    internal sealed class DeleteOperation : OperationBase<object>
+    {
+        public DeleteOperation(string key, IVBucket vBucket)
+            : base(key, vBucket)
+        {
+        }
+
+        public override OperationCode OperationCode
+        {
+            get { return OperationCode.Delete; }
+        }
+
+        public override ArraySegment<byte> CreateExtras()
+        {
+            return new ArraySegment<byte>();
+        }
+    }
+}
