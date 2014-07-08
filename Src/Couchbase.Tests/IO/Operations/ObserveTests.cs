1765c40b1f65997e8f37c7e1b0c119eb18990f12
jeffrymorris <jeffrymorris@gmail.com>
2014-07-08 15:08:42 -0700

    NCBC-541: Implement Observe command

---
+++ Src/Couchbase.Tests/IO/Operations/ObserveTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Couchbase.IO.Converters;
+using Couchbase.IO.Operations;
+using NUnit.Framework;
+
+namespace Couchbase.Tests.IO.Operations
+{
+    [TestFixture]
+    public class ObserveTests : OperationTestBase
+    {
+        [Test]
+        public void Test_Observe()
+        {
+            const string key = "Test_Observe";
+
+            var operation = new Observe(key, GetVBucket(), new AutoByteConverter());
+            var result = IOStrategy.Execute(operation);
+            Console.WriteLine(result.Message);
+            Assert.IsTrue(result.Success);
+        }
+
+        [Test]
+        public void Test_Observe2()
+        {
+            const string key = "Test_Observe";
+
+            var operation = new Observe(key, GetVBucket(), new AutoByteConverter());
+            var result = IOStrategy.Execute(operation);
+            Console.WriteLine(result.Message);
+            Console.WriteLine(result.Value);
+            Assert.IsTrue(result.Success);
+        }
+    }
+}
---
+++ Src/Couchbase/IO/Operations/Observe.cs
@@ -0,0 +1,97 @@
+﻿using Couchbase.Core;
+using Couchbase.Core.Serializers;
+using Couchbase.IO.Converters;
+using Couchbase.IO.Utils;
+
+namespace Couchbase.IO.Operations
+{
+    internal sealed class Observe : OperationBase<ObserveState>
+    {
+        public Observe(IByteConverter converter)
+            : base(converter)
+        {
+        }
+
+        public Observe(string key, ITypeSerializer serializer, IVBucket vBucket, IByteConverter converter)
+            : base(key, null, serializer, vBucket, converter)
+        {
+        }
+
+        public Observe(string key, IVBucket vBucket, IByteConverter converter)
+            : base(key, vBucket, converter)
+        {
+        }
+
+        public Observe(string key, IVBucket vBucket, IByteConverter converter, ITypeSerializer serializer)
+            : base(key, vBucket, converter, serializer)
+        {
+        }
+
+        public override byte[] Write()
+        {
+            var body = new byte[2 + 2 + Key.Length];
+            Converter.FromInt16((short)VBucket.Index, body, 0);
+            Converter.FromInt16((short)Key.Length, body, 2);
+            Converter.FromString(Key, body, 4);
+
+            var header = new byte[24];
+            Converter.FromByte((byte)Magic.Request, header, HeaderIndexFor.Magic);
+            Converter.FromByte((byte)OperationCode, header, HeaderIndexFor.Opcode);
+            Converter.FromInt32(body.Length, header, HeaderIndexFor.BodyLength);
+            Converter.FromInt32(Opaque, header, HeaderIndexFor.Opaque);
+
+            var buffer = new byte[body.Length + header.Length];
+            System.Buffer.BlockCopy(header, 0, buffer, 0, header.Length);
+            System.Buffer.BlockCopy(body, 0, buffer, header.Length, body.Length);
+            return buffer;
+        }
+
+        public override ObserveState GetValue()
+        {
+            var buffer = Data.GetBuffer();
+            var keylength = Converter.ToInt16(buffer, 26);
+
+            return new ObserveState
+            {
+                VBucket = Converter.ToInt16(buffer, 24),
+                KeyLength = keylength,
+                Key = Converter.ToString(buffer, 28, keylength),
+                KeyState = (KeyState)Converter.ToByte(buffer, 33),
+                Cas = Converter.ToUInt64(buffer, 34)
+            };
+        }
+
+        public override IOperationResult<ObserveState> GetResult()
+        {
+            return new OperationResult<ObserveState>(this);
+        }
+
+        public override OperationCode OperationCode
+        {
+            get { return OperationCode.Observe; }
+        }
+    }
+}
+
+#region [ License information ]
+
+/* ************************************************************
+ *
+ *    @author Couchbase <info@couchbase.com>
+ *    @copyright 2014 Couchbase, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ * ************************************************************/
+
+#endregion [ License information ]
