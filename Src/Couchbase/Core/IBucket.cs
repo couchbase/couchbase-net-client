1de96312065180604130d3f306c87a884125a3bc
jeffrymorris <jeffrymorris@gmail.com>
2014-07-13 15:53:12 -0700

    Comments and renaming

--- Src/Couchbase/Core/IBucket.cs
+++ Src/Couchbase/Core/IBucket.cs
@@ -270,8 +270,16 @@ namespace Couchbase.Core
         /// <returns>An <see cref="T:Couchbase.Views.IViewQuery"/> which can have more filters and options applied to it.</returns>
         IViewQuery CreateQuery(bool development, string designdoc, string view);
 
-        int Retain();
-
+        /// <summary>
+        /// Increments the reference counter for this <see cref="IBucket"/> instance.
+        /// </summary>
+        /// <returns>The current count of all <see cref="IBucket"/> references.</returns>
+        int AddRef();
+        
+        /// <summary>
+        /// Decrements the reference counter and calls <see cref="IDisposable.Dispose"/> if the count is zero.
+        /// </summary>
+        /// <returns></returns>
         int Release();
     }
 }
