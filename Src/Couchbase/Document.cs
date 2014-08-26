cfd6e320ae10b1695ea8ebdd41357a778e891ab2
jeffrymorris <jeffrymorris@gmail.com>
2014-08-26 16:12:08 -0700

    Added code comments to IDocument and Document
    Change-Id: Idaee34a2b6707c4fd4477e4d606094199ac33986
    Reviewed-on: http://review.couchbase.org/40950
    Tested-by: Jeffry Morris <jeffrymorris@gmail.com>
    Reviewed-by: Brett Lawson <brett19@gmail.com>

--- Src/Couchbase/Document.cs
+++ Src/Couchbase/Document.cs
@@ -6,14 +6,31 @@ using System.Threading.Tasks;
 
 namespace Couchbase
 {
+    /// <summary>
+    /// Provides an interface for interacting with documents within Couchbase Server
+    /// </summary>
+    /// <typeparam name="T">The type of document.</typeparam>
     public class Document<T> : IDocument<T>
     {
+        /// <summary>
+        /// The unique identifier for the document
+        /// </summary>
         public string Id { get; set; }
 
+        /// <summary>
+        /// The "Check and Set" value for enforcing optimistic concurrency
+        /// </summary>
         public ulong Cas { get; set; }
 
+        /// <summary>
+        /// The time-to-live or TTL for the document before it's evicated from disk
+        /// </summary>
+        /// <remarks>Setting this to zero or less will give the document infinite lifetime</remarks>
         public uint Expiry { get; set; }
 
+        /// <summary>
+        /// The value representing the document itself
+        /// </summary>
         public T Value { get; set; }
     }
 }
--- Src/Couchbase/IDocument'.cs
+++ Src/Couchbase/IDocument'.cs
@@ -6,14 +6,31 @@ using System.Threading.Tasks;
 
 namespace Couchbase
 {
+    /// <summary>
+    /// Provides an interface for interacting with documents within Couchbase Server
+    /// </summary>
+    /// <typeparam name="T">The type of document.</typeparam>
     public interface IDocument<T>  
     {
+        /// <summary>
+        /// The unique identifier for the document
+        /// </summary>
         string Id { get; set; }
 
+        /// <summary>
+        /// The "Check and Set" value for enforcing optimistic concurrency
+        /// </summary>
         ulong Cas { get; set; }
 
+        /// <summary>
+        /// The time-to-live or TTL for the document before it's evicated from disk
+        /// </summary>
+        /// <remarks>Setting this to zero or less will give the document infinite lifetime</remarks>
         uint Expiry { get; set; }
 
+        /// <summary>
+        /// The value representing the document itself
+        /// </summary>
         T Value { get; set; }
     }
 }
