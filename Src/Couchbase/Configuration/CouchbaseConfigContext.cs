couchbase/couchbase-net-client

commit 48023fad3d73192e26dc5df0f570e6e1757a21a2

NCBC-663: Authentication error when making View request to bucket with SASL Auth
View requests were not using the bucket credentials when making requests
against password protected buckets. This commit fixes that by passing the
bucket configuration to the ViewClient and then adding the credentials to
the HTTP request header.

Change-Id: Icec8a8db8d7379d894f04e43c445d0f67bf5ffc5
Reviewed-on: http://review.couchbase.org/41838
Tested-by: Jeffry Morris <jeffrymorris@gmail.com>
Reviewed-by: Brett Lawson <brett19@gmail.com>

Src/Couchbase/Configuration/CouchbaseConfigContext.cs
@@ -59,7 +59,7 @@ namespace Couchbase.Configuration
                             var saslMechanism = SaslFactory(bucketConfig.Name, bucketConfig.Password, ioStrategy,
                                 Converter);
                             ioStrategy.SaslMechanism = saslMechanism;
-                            var server = new Core.Server(ioStrategy, nodes[i], ClientConfig);
+                            var server = new Core.Server(ioStrategy, nodes[i], ClientConfig, bucketConfig);
                             servers.Add(server);
                         }
                         catch (Exception e)
@@ -105,7 +105,7 @@ namespace Couchbase.Configuration
                         IServer server = null;
                         if (Equals(ioStrategy.EndPoint, endpoint) || nodes.Length == 1)
                         {
-                            server = new Core.Server(ioStrategy, node, ClientConfig);
+                            server = new Core.Server(ioStrategy, node, ClientConfig, BucketConfig);
                         }
                         else
                         {
@@ -115,7 +115,7 @@ namespace Couchbase.Configuration
                             var saslMechanism = SaslFactory(BucketConfig.Name, BucketConfig.Password,
                                 newIoStrategy, Converter);
                             newIoStrategy.SaslMechanism = saslMechanism;
-                            server = new Core.Server(newIoStrategy, nodes[i], ClientConfig);
+                            server = new Core.Server(ioStrategy, nodes[i], ClientConfig, BucketConfig);
                         }
                         servers.Add(server);
                     }
@@ -163,7 +163,7 @@ namespace Couchbase.Configuration
                         var ioStrategy = IOStrategyFactory(connectionPool);
                         var saslMechanism = SaslFactory(BucketConfig.Name, BucketConfig.Password, ioStrategy, Converter);
                         ioStrategy.SaslMechanism = saslMechanism;
-                        var server = new Core.Server(ioStrategy, nodes[i], ClientConfig);
+                        var server = new Core.Server(ioStrategy, nodes[i], ClientConfig, BucketConfig);
                             //this should be a Func factory...a functory
                         servers.Add(server);
                     }

Src/Couchbase.Tests/Core/Buckets/CouchbaseBucketTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Authentication;
 using System.Threading;
@@ -8,6 +9,7 @@ using Couchbase.Configuration;
 using Couchbase.Core;
 using Couchbase.IO;
 using Couchbase.IO.Operations;
+using Couchbase.Management;
 using Couchbase.Views;
 using Moq;
 using NUnit.Framework;
@@ -77,6 +79,24 @@ namespace Couchbase.Tests.Core.Buckets
             using (var bucket = _cluster.OpenBucket("authenicated", "secret"))
             {
                 Assert.IsNull(bucket);
+            }
+        }
+
+        [Test]
+        public void Test_View_Query_Authenticated()
+        {
+            using (var bucket = _cluster.OpenBucket("authenticated", "secret"))
+            {
+                var manager = bucket.CreateManager("Administrator", "password");
+                manager.InsertDesignDocument("docs", File.ReadAllText(@"Data\\DesignDocs\\docs.json"));
+                var query = bucket.CreateQuery("docs", "all_docs").
+                    Development(false).
+                    Limit(10);
+
+                Console.WriteLine(query.RawUri());
+                var result = bucket.Query<dynamic>(query);
+                Assert.AreEqual("", result.Message);
+                Assert.Greater(result.TotalRows, 0);
             }
         }
 

Src/Couchbase.Tests/Couchbase.Tests.csproj
@@ -183,6 +183,9 @@
     <None Include="Data\Configuration\vbucket-forwardmap.json">
       <CopyToOutputDirectory>Always</CopyToOutputDirectory>
     </None>
+    <None Include="Data\DesignDocs\docs.json">
+      <CopyToOutputDirectory>Always</CopyToOutputDirectory>
+    </None>
     <None Include="Data\DesignDocs\by_field2.json">
       <CopyToOutputDirectory>Always</CopyToOutputDirectory>
     </None>

Src/Couchbase/Configuration/Server/Providers/CarrierPublication/CarrierPublicationProvider.cs
@@ -28,7 +28,7 @@ namespace Couchbase.Configuration.Server.Providers.CarrierPublication
         {
             _heartBeat = new Timer
             {
-                Interval = ClientConfig.ConfigHeartbeatInterval,
+                Interval = ClientConfig.HeartbeatConfigInterval,
                 Enabled = ClientConfig.EnableConfigHeartBeat,
                 AutoReset = true
             };
@@ -80,6 +80,7 @@ namespace Couchbase.Configuration.Server.Providers.CarrierPublication
                         {
                             var bucketConfig = operationResult.Value;
                             bucketConfig.SurrogateHost = connectionPool.EndPoint.Address.ToString();
+                            bucketConfig.Password = password;
                             configInfo = new CouchbaseConfigContext(bucketConfig,
                                 ClientConfig,
                                 IOStrategyFactory,

Src/Couchbase/Configuration/Server/Providers/Streaming/HttpStreamingProvider.cs
@@ -70,7 +70,7 @@ namespace Couchbase.Configuration.Server.Providers.Streaming
                             var body = webClient.DownloadString(uri);
                             newConfig = JsonConvert.DeserializeObject<BucketConfig>(body);
                         }
-
+                        newConfig.Password = password;
                         configInfo = CreateConfigInfo(newConfig);
                         Configs[bucketName] = configInfo;
                         break;

Src/Couchbase/Core/Server.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using Common.Logging;
 using Couchbase.Configuration;
@@ -32,6 +33,15 @@ namespace Couchbase.Core
             new QueryClient(new HttpClient(), new JsonDataMapper()),
             node, clientConfiguration)
         {
+        }
+
+        public Server(IOStrategy ioStrategy, Node node, ClientConfiguration clientConfiguration, IBucketConfig bucketConfig) :
+            this(ioStrategy,
+            new ViewClient(new HttpClient(), new JsonDataMapper(), bucketConfig),
+            new QueryClient(new HttpClient(), new JsonDataMapper()),
+            node, clientConfiguration)
+        {
+
         }
 
         public Server(IOStrategy ioStrategy, IViewClient viewClient, IQueryClient queryClient, Node nodeInfo, ClientConfiguration clientConfiguration)

Src/Couchbase/Views/ViewClient.cs
@@ -4,8 +4,11 @@ using System.Globalization;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Common.Logging;
+using Couchbase.Configuration.Server.Serialization;
 
 namespace Couchbase.Views
 {
@@ -16,10 +19,21 @@ namespace Couchbase.Views
     {
         const string Success = "Success";
         private readonly static ILog Log = LogManager.GetCurrentClassLogger();
+        private readonly IBucketConfig _bucketConfig;
 
         public ViewClient(HttpClient httpClient, IDataMapper mapper)
         {
             HttpClient = httpClient;
+            Mapper = mapper;
+        }
+
+        public ViewClient(HttpClient httpClient, IDataMapper mapper,  IBucketConfig bucketConfig)
+        {
+            _bucketConfig = bucketConfig;
+            HttpClient = httpClient;
+            HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                "Basic", Convert.ToBase64String(
+                Encoding.UTF8.GetBytes(string.Concat(_bucketConfig.Name, ":", _bucketConfig.Password))));
             Mapper = mapper;
         }
 
@@ -36,7 +50,6 @@ namespace Couchbase.Views
             {
                 var result = await HttpClient.GetAsync(query.RawUri());
                 var content = result.Content;
-                
                 var stream = await content.ReadAsStreamAsync();
 
                 viewResult = Mapper.Map<ViewResult<T>>(stream);
@@ -68,17 +81,15 @@ namespace Couchbase.Views
             var viewResult = new ViewResult<T>();
             try
             {
-                var task = HttpClient.GetAsync(query.RawUri());
-                task.Wait();
-                var result = task.Result;
+                var requestAwaiter = HttpClient.GetAsync(query.RawUri()).ConfigureAwait(false).GetAwaiter();
+                var request = requestAwaiter.GetResult();
 
-                var content = result.Content;
-                var stream = content.ReadAsStreamAsync();
-                stream.Wait();
+                var responseAwaiter = request.Content.ReadAsStreamAsync().ConfigureAwait(false).GetAwaiter();
+                var response = responseAwaiter.GetResult();
+                viewResult = Mapper.Map<ViewResult<T>>(response);
 
-                viewResult = Mapper.Map<ViewResult<T>>(stream.Result);
-                viewResult.Success = result.IsSuccessStatusCode;
-                viewResult.StatusCode = result.StatusCode;
+                viewResult.Success = request.IsSuccessStatusCode;
+                viewResult.StatusCode = request.StatusCode;
             }
             catch (AggregateException ae)
             {

commit 690fc41a8a36948127da99343913b7adb3c4a434

NCBC-665: Significant regression in memcached ops
This patch fixes a regression but that was introduced in commit
48023fad3d73192e26dc5df0f570e6e1757a21a2.

Change-Id: Icb10d902f7574b4e6c4133d107ded501feb7eebd
Reviewed-on: http://review.couchbase.org/41851
Tested-by: Jeffry Morris <jeffrymorris@gmail.com>
Reviewed-by: Brett Lawson <brett19@gmail.com>

Src/Couchbase/Configuration/CouchbaseConfigContext.cs
@@ -115,7 +115,7 @@ namespace Couchbase.Configuration
                             var saslMechanism = SaslFactory(BucketConfig.Name, BucketConfig.Password,
                                 newIoStrategy, Converter);
                             newIoStrategy.SaslMechanism = saslMechanism;
-                            server = new Core.Server(ioStrategy, nodes[i], ClientConfig, BucketConfig);
+                            server = new Core.Server(newIoStrategy, nodes[i], ClientConfig, BucketConfig);
                         }
                         servers.Add(server);
                     }