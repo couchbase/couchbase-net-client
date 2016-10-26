5d8de528c61e3bf1514bb1869a2b0130ed4597cc
MikeGoldsmith <goldsmith.mike@gmail.com>
2016-10-26 13:23:48 +0100

    NCBC-1225: PoolConfigurationTests do not match min and max values
    MOTIVATION
    ----------
    The PoolConfigurationTests maximum values are out of date.
    They are testing for a maximum of 20, where the maximum is 500.
    
    MODIFICATIONS
    -------------
    Updated the test values to test the 500 boundary.
    
    RESULT
    ------
    The Tests now test the maximum connection value boundary correctly.
    
    Change-Id: I3a5b805e60f0317c7fc7d9b2339d90017dbfa60b
    Reviewed-on: http://review.couchbase.org/69239
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Jeffry Morris <jeffrymorris@gmail.com>

--- Src/Couchbase.UnitTests/Configuration/Client/PoolConfigurationTests.cs
+++ Src/Couchbase.UnitTests/Configuration/Client/PoolConfigurationTests.cs
@@ -8,11 +8,11 @@ namespace Couchbase.UnitTests.Configuration.Client
     public class PoolConfigurationTests
     {
         [TestCase(0, 1, Description = "MaxSize is less than 1")]
-        [TestCase(21, 1, Description = "MaxSize is greater than 20")]
+        [TestCase(501, 1, Description = "MaxSize is greater than 500")]
         [TestCase(1, 0, Description = "MinSize is less than 1")]
-        [TestCase(1, 21, Description = "MinSize is greater than 20")]
+        [TestCase(501, 501, Description = "MinSize is greater than 500")]
         [TestCase(5, 10, Description = "Maxsize is greater than MinSize")]
-        public void THorws_Argument_Exception_If_MaxSize_Is_Not_Valid(int maxSize, int minSize)
+        public void Throws_Argument_Exception_If_Connection_Values_Are_Not_Valid(int maxSize, int minSize)
         {
             Assert.Throws<ArgumentOutOfRangeException>(() => new PoolConfiguration(maxSize, minSize));
         }
