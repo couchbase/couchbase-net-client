3215554a698bac67263bec05bffa53679c2d911c
MikeGoldsmith <goldsmith.mike@gmail.com>
2016-12-21 14:22:40 +0000

    NCBC-1254: Revert IQueryResult<T> refactoring
    MOTIVATION
    ----------
    As part of NCBC-1079 the IQueryResult<T> interface was refactored with a
    new IQueryResult interface being created to hold the non-generic
    properties. However, this broke integration projects that used the public
    interface explicitly.
    
    MODIFICATIONS
    -------------
    - Return the properties that were moved to IQueryResult to
      IQueryResult<T>
    - Remove IQueryResult
    - Replace direct IQueryResult usage with IQueryResult<T>
    
    RESULT
    ------
    The IQueryResult<T> interface is now in the expected state and
    integration projects, such as Linq2Couchbase, can use this library.
    
    Change-Id: I7703ae9435d0dfcf28ea63e5a660575a495f7e09
    Reviewed-on: http://review.couchbase.org/71188
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Mike Goldsmith <goldsmith.mike@gmail.com>

--- Src/Couchbase/N1QL/IQueryResult'.cs
+++ Src/Couchbase/N1QL/IQueryResult'.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using Couchbase.N1QL;
-using Newtonsoft.Json;
 
 namespace Couchbase.N1QL
 {
@@ -9,7 +7,7 @@ namespace Couchbase.N1QL
     /// Interface for the results of a N1QL query.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public interface IQueryResult<T> : IEnumerable<T>, IQueryResult
+    public interface IQueryResult<T> : IEnumerable<T>, IResult, IDisposable
     {
         /// <summary>
         /// Gets a list of all the objects returned by the query. An object can be any JSON value.
@@ -18,6 +16,62 @@ namespace Couchbase.N1QL
         /// A a list of all the objects returned by the query.
         /// </value>
         List<T> Rows { get; }
+
+        /// <summary>
+        /// Gets A unique identifier for the response.
+        /// </summary>
+        /// <value>
+        /// The unique identifier for the response.
+        /// </value>
+        Guid RequestId { get; }
+
+        /// <summary>
+        /// Gets the clientContextID of the request, if one was supplied. Used for debugging.
+        /// </summary>
+        /// <value>
+        /// The client context identifier.
+        /// </value>
+        string ClientContextId { get; }
+
+        /// <summary>
+        /// Gets the schema of the results. Present only when the query completes successfully.
+        /// </summary>
+        /// <value>
+        /// The signature of the schema of the request.
+        /// </value>
+        dynamic Signature { get; }
+
+        /// <summary>
+        /// Gets the status of the request; possible values are: success, running, errors, completed, stopped, timeout, fatal.
+        /// </summary>
+        /// <value>
+        /// The status of the request.
+        /// </value>
+        QueryStatus Status { get; }
+
+        /// <summary>
+        /// Gets a list of 0 or more error objects; if an error occurred during processing of the request, it will be represented by an error object in this list.
+        /// </summary>
+        /// <value>
+        /// The errors.
+        /// </value>
+        List<Error> Errors { get; }
+
+        /// <summary>
+        /// Gets a list of 0 or more warning objects; if a warning occurred during processing of the request, it will be represented by a warning object in this list.
+        /// </summary>
+        /// <value>
+        /// The warnings.
+        /// </value>
+        List<Warning> Warnings { get; }
+
+        /// <summary>
+        /// Gets an object containing metrics about the request.
+        /// </summary>
+        /// <value>
+        /// The metrics.
+        /// </value>
+        Metrics Metrics { get; }
     }
 }
 #region [ License information ]
