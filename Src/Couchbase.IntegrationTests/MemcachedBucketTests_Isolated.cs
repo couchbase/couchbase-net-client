da266acf267c717dac41070db6a5e4d82be14354
jeffrymorris <jeffrymorris@gmail.com>
2017-01-26 15:56:30 -0800

    NCBC-1292: Migrate memcached tests to integration test project
    Motivation
    ----------
    Move memcached tests from Couchbase.Test (obsolete) to
    Couchbase.IntegrationTests so that we can run them to check for
    regressions.
    
    Modifications
    -------------
     - Copy MemcachedBucketTests.cs and MemcachedBucketTests_isolated.cs to
     Couchbase.IntegrationTests.
     - Add MemcachedBucket_Test to MemcachedBucketTests
     - Delete MemcachedBucket_Tests.cs
    
    Results
    -------
    We now have Memcached integration tests.
    
    Change-Id: I11cb15b35b0ff8afbd22a9cd3cf6cfcaf2cbff28
    Reviewed-on: http://review.couchbase.org/72549
    Tested-by: Jeffry Morris <jeffrymorris@gmail.com>
    Reviewed-by: Mike Goldsmith <goldsmith.mike@gmail.com>

---
+++ Src/Couchbase.IntegrationTests/MemcachedBucketTests_Isolated.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Couchbase.IntegrationTests
+{
+// ReSharper disable once InconsistentNaming
+    [TestFixture]
+    public class MemcachedBucketTests_Isolated
+    {
+        [Test]
+        [Category("Integration")]
+        [Category("Memcached")]
+        public void When_Bucket_Goes_Out_of_Scope_Process_Will_Not_Hang()
+        {
+            var config = Utils.TestConfiguration.GetCurrentConfiguration();
+
+            //if the configuration thread is a foreground thread, this would hang indefinitly
+            new Cluster(config).OpenBucket("memcached");
+        }
+    }
+}
