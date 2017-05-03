b0cbde9d6770c33238bd3845c90c6834902c96ce
Mike Goldsmith <goldsmith.mike@gmail.com>
2017-05-03 19:03:46 +0100

    NCBC-1412: Add binary transcoder
    MOTIVATION
    ----------
    It would be useful to have an alternative transcoder to the default JSON
    one. This commit adds a binary transcoder that can store / retrieve data
    stored as raw bytes.
    
    MODIFICATIONS
    -------------
    - Add BinaryTranscoder
    - Add BinaryTranscoderTests integration test
    
    RESULT
    ------
    There is an alternative Trancoder that can store documents as raw bytes
    instead of using JSON.
    
    Change-Id: I9a2746b3c55c2a2df4d81aacffcac79817179d51
    Reviewed-on: http://review.couchbase.org/77673
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Jeffry Morris <jeffrymorris@gmail.com>

---
+++ Src/Couchbase/Core/Transcoders/BinaryTranscoder.cs
@@ -0,0 +1,44 @@
+﻿#if NET45
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Couchbase.Core.Transcoders
+{
+    /// <summary>
+    /// A binary level transcoder that can serialize / deserialize binary arrays using <see cref="BinaryFormatter"/>.
+    /// </summary>
+    /// <remarks>Any custom classes must have the <see cref="SerializableAttribute"/>.</remarks>
+    public class BinaryTranscoder : DefaultTranscoder
+    {
+        private readonly BinaryFormatter _formatter = new BinaryFormatter();
+
+        public override T DeserializeAsJson<T>(byte[] buffer, int offset, int length)
+        {
+            if (length == 0 || buffer.Length < offset + length)
+            {
+                return default(T);
+            }
+
+            using (var stream = new MemoryStream(buffer, offset, length))
+            {
+                return (T) _formatter.Deserialize(stream);
+            }
+        }
+
+        public override byte[] SerializeAsJson(object value)
+        {
+            if (value == null)
+            {
+                return new byte[0];
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                _formatter.Serialize(ms, value);
+                return ms.ToArray();
+            }
+        }
+    }
+}
+#endif
