8e6774731c0492467f3bf4230a3a1eb5efb13c1e
Mike Goldsmith <goldsmith.mike@gmail.com>
2017-07-14 16:19:30 +0100

    NCBC-1323: Update appveyor to support VS2017 projects
    MOTIVATION
    ----------
    After the VS 2017 project file update, the appveyor config requires
    updating to allow it to successfully build the solution.
    
    MODIFICATIONS
    -------------
    - update appveyor.yml to use a local build script instead of inline
      commands
    - add build.ps1 that determines version, decrpyts snk, updates
      assemblyinfo.cs and builds the project
    - add AutoVersionNumber.ps1 to support working out next version
    
    RESULT
    ------
    The updated project can now be built and auto increments it's version
    without requiring updating.
    
    Change-Id: I30f7a1dba5d34448eab2836d0f2e7edce2a40692
    Reviewed-on: http://review.couchbase.org/80924
    Reviewed-by: Brant Burnett <bburnett@centeredgesoftware.com>
    Reviewed-by: Jeffry Morris <jeffrymorris@gmail.com>
    Tested-by: Build Bot <build@couchbase.com>

---
+++ build-utils/Build.ps1
@@ -0,0 +1,23 @@
+﻿# is this a tagged build?
+if ($env:APPVEYOR_REPO_TAG -eq "true") {
+    # use tag as version
+    $versionNumber = "$env:APPVEYOR_REPO_TAG_NAME"
+} else {
+    # create pre-release build number based on AppVeyor build number
+    $buildCounter = "$env:APPVEYOR_BUILD_NUMBER".PadLeft(6, "0")
+    $versionNumber = .\build-utils\AutoVersionNumber.ps1 -VersionSuffix "alpha-$buildCounter"
+}
+
+# replace AssemblyInfo with version that doesn't include IntervalsVisibleTo attributes
+Copy-Item .\build-utils\AssemblyInfo.cs .\Src\Couchbase\Properties\AssemblyInfo.cs -Force
+
+# decrypt snk for signing the assembly
+nuget install secure-file -ExcludeVersion
+.\secure-file\tools\secure-file.exe -decrypt .\build-utils\Couchbase.snk.enc -secret $env:SnkSecret -out .\Src\Couchbase\Couchbase.snk
+
+# clean then build with snk & version number creating nuget package
+msbuild Src\Couchbase\Couchbase.csproj /t:Clean /p:Configuration=Release
+msbuild Src\Couchbase\Couchbase.csproj /t:Restore,Pack /p:Configuration=Release /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=Couchbase.snk /p:version=$versionNumber /p:PackageOutputPath=..\..\
+
+# create zip from release folder
+Compress-Archive -Path .\Src\Couchbase\bin\Release\* -CompressionLevel Optimal -DestinationPath .\Couchbase-Net-Client-$versionNumber.zip
