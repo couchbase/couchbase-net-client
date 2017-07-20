558ad097419c838008dfc8986af3ae8860c83500
jeffrymorris <jeffrymorris@gmail.com>
2017-07-19 17:25:08 -0700

    NCBC-1468: Update Couchbase so that it can be use by SDKD and VS2017
    Motivation
    ----------
    A few minor changes required for SDKD.NET so that it can run siti tests
    against Core/Full.
    
    Modifications
    -------------
     - Remove conditional compilation for internalInvisibleTo
     - Remove csproj elements for creating attributes; this keeps the
       IDE/tooling from generating duplicate attributes and failing on build.
    
    Results
    -------
    SDKD.NET can use the correct CLR version for testing.
    
    Change-Id: I8053751482a3f88e07931d04657616f78e264e0e
    Reviewed-on: http://review.couchbase.org/81144
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Mike Goldsmith <goldsmith.mike@gmail.com>

--- Src/Couchbase/Properties/AssemblyInfo.cs
+++ Src/Couchbase/Properties/AssemblyInfo.cs
@@ -1,16 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Couchbase Inc.")]
-[assembly: AssemblyProduct("Couchbase")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -27,10 +16,9 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("Couchbase.Linq")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 
-#if NET45
+//for siti testing
 [assembly: InternalsVisibleTo("Sdkd")]
 [assembly: InternalsVisibleTo("SdkdConsole")]
-#else
 [assembly: InternalsVisibleTo("Sdkd.NetStandard")]
 [assembly: InternalsVisibleTo("SdkdConsole.NetStandard")]
-#endif
+
