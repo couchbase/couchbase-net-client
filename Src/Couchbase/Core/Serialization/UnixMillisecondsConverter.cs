cc128818b97beaa060fbaab44df864301809c2a5
Brant Burnett <bburnett@centeredgesoftware.com>
2018-03-16 10:30:36 -0400

    NCBC-1591: Add Newtonsoft helper Unix epoch milliseconds attrs
    Motivation
    ----------
    Two very common ways of serializing date/times to JSON in Couchbase are
    ISO 8601 and Unix epoch milliseconds.  The default behavior for the .NET
    SDK is ISO 8601, but an easy and standardized way for developers to use
    Unix timestamps would be useful.  This method could then be detected by
    Linq2Couchbase to adjust query behavior.
    
    Modifications
    -------------
    Added UnixMillisecondsConverter, a Newtonsoft JsonConverter, and
    supporting unit tests.
    
    Results
    -------
    Developers may apply the converter to any DateTime attribute, or
    globally in their JsonSerializerSettings.
    
    ```cs
    [JsonConverter(typeof(UnixMillisecondsConverter))]
    public DateTime Timestamp { get; set; }
    ```
    
    This results in both serializing and deserializing the property as
    milliseconds since the Unix epoch.  Conversion to UTC is handled
    automatically for local times.  Fractions of a millisecond are
    preserved, up to the resolution of TimeSpan.TotalMilliseconds.
    
    This attribute may then be detected by Linq2Couchbase to adjust query
    generation for this properties.
    
    Change-Id: I2b98799edbc0637b608cb55dc5cf6b53c33c1890
    Reviewed-on: http://review.couchbase.org/91085
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Mike Goldsmith <goldsmith.mike@gmail.com>

---
+++ Src/Couchbase/Core/Serialization/UnixMillisecondsConverter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Couchbase.Core.Serialization
+{
+    /// <summary>
+    /// <see cref="JsonConverter"/> which serializes a <see cref="DateTime"/> property as
+    /// milliseconds since the Unix epoch.  This is an alternative to the default ISO8601 format.
+    /// </summary>
+    /// <remarks>
+    /// Apply to a property using <see cref="JsonConverterAttribute"/>.
+    /// </remarks>
+    public class UnixMillisecondsConverter : JsonConverter
+    {
+        private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(DateTime);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+            {
+                return null;
+            }
+            else
+            {
+                return UnixEpoch.AddMilliseconds(Convert.ToDouble(reader.Value));
+            }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var dateTime = (DateTime) value;
+
+            if (dateTime.Kind == DateTimeKind.Local)
+            {
+                dateTime = dateTime.ToUniversalTime();
+            }
+
+            var unixMilliseconds = (dateTime - UnixEpoch).TotalMilliseconds;
+            // ReSharper disable once CompareOfFloatsByEqualityOperator
+            if (Math.Floor(unixMilliseconds) == unixMilliseconds)
+            {
+                // No partial milliseconds, so serialize as an integer
+                // This prevents an unnecessary ".0" in the output
+
+                writer.WriteValue((long) unixMilliseconds);
+            }
+            else
+            {
+                // Has partial milliseconds, so let's keep them
+                writer.WriteValue(unixMilliseconds);
+            }
+        }
+    }
+}
