6e69f47b1eb8b682d0a250bc2168877b440253d2
Rune Reisegg Jacobsen <rune@pana.no>
2018-05-30 12:56:15 +0200

    Fix documentation of Expiry
    The value is in seconds, not milliseconds.
    https://developer.couchbase.com/documentation/server/4.1/developer-guide/expiry.html

--- Src/Couchbase/Document.cs
+++ Src/Couchbase/Document.cs
@@ -21,7 +21,7 @@ namespace Couchbase
         public ulong Cas { get; set; }
 
         /// <summary>
-        /// The time-to-live or TTL for the document before it's evicted from disk in milliseconds.
+        /// The time-to-live or TTL for the document before it's evicted from disk in seconds.
         /// </summary>
         /// <remarks>Setting this to zero or less will give the document infinite lifetime</remarks>
         public uint Expiry { get; set; }
