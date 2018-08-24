b45d4a91cabe15a9f5a5716e9e77f6c801998d60
Dirk Lemstra <dirk@git.imagemagick.org>
2018-08-24 20:27:30 +0200

    NCBC-1759: Fix expiration bugs in CouchbaseBucket Increment and IncrementAsync
    MOTIVATION
    ----------
    Some of the Increment and IncrementAsync overloads set the timeout as
    the expiration. Another overload set an incorrect timeout that would
    cause TimeSpan.FromMinutes(5) to become 00:00:00.0000300. This would
    then cause the expire time of the document to become zero (infinity)
    instead of 5 minutes.
    
    MODIFICATIONS
    -------------
    - Set GlobalTimeout for unit testing.
    - Made some of the fields in Increment public readonly instead of a
      private field for unit testing.
    - Refactored some of the code to make it more clear what is passed to
      the other overloads.
    - Created separate files for testing the Increment and IncrementAsync
      methods.
    
    RESULT
    ------
    The Increment and IncrementAsync method will set the correct
    expiration.
    
    Change-Id: I474713a8f9a713d57658f0ff4c2d790ba3efbd23
    Reviewed-on: http://review.couchbase.org/98792
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Jeffry Morris <jeffrymorris@gmail.com>

---
+++ Src/Couchbase.UnitTests/CouchbaseBucketTests/IncrementAsyncTests.cs
@@ -0,0 +1,308 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Couchbase.Core.Buckets;
+using Couchbase.Core.Transcoders;
+using Couchbase.IO.Converters;
+using Couchbase.IO.Operations;
+using Moq;
+using NUnit.Framework;
+
+// ReSharper disable once CheckNamespace
+namespace Couchbase.UnitTests
+{
+    [TestFixture]
+    public class IncrementAsyncTests
+    {
+        private readonly IByteConverter _converter = new DefaultConverter();
+        private readonly ITypeTranscoder _transcoder = new DefaultTranscoder();
+
+        [Test]
+        public async Task IncrementAsync_With_Key_ExecutesCorrectOperation()
+        {
+            // Arrange
+
+            Increment operation = null;
+
+            var mockRequestExecuter = new Mock<IRequestExecuter>();
+            mockRequestExecuter
+                .Setup(m => m.SendWithRetryAsync(It.IsAny<Increment>(), null, null))
+                .Callback((IOperation<ulong> op, TaskCompletionSource<IOperationResult<ulong>> tcs, CancellationTokenSource ccs) => operation = (Increment)op)
+                .ReturnsAsync((IOperationResult<ulong>)null);
+
+            // Act
+
+            using (var bucket = new CouchbaseBucket(mockRequestExecuter.Object, _converter, _transcoder))
+            {
+                bucket.Name = "bucket";
+
+                await bucket.IncrementAsync("key");
+            }
+
+            // Assert
+
+            Assert.NotNull(operation);
+            Assert.AreEqual(1, operation.Delta);
+            Assert.AreEqual(1, operation.Initial);
+            Assert.AreEqual("bucket", operation.BucketName);
+            Assert.AreEqual(0, operation.Expires);
+            Assert.AreEqual(2, operation.Timeout);
+        }
+
+        [Test]
+        public async Task IncrementAsync_With_Timeout_ExecutesCorrectOperation()
+        {
+            // Arrange
+
+            Increment operation = null;
+
+            var mockRequestExecuter = new Mock<IRequestExecuter>();
+            mockRequestExecuter
+                .Setup(m => m.SendWithRetryAsync(It.IsAny<Increment>(), null, null))
+                .Callback((IOperation<ulong> op, TaskCompletionSource<IOperationResult<ulong>> tcs, CancellationTokenSource ccs) => operation = (Increment)op)
+                .ReturnsAsync((IOperationResult<ulong>)null);
+
+            // Act
+
+            using (var bucket = new CouchbaseBucket(mockRequestExecuter.Object, _converter, _transcoder))
+            {
+                bucket.Name = "bucket";
+
+                await bucket.IncrementAsync("key", TimeSpan.FromSeconds(10));
+            }
+
+            // Assert
+
+            Assert.NotNull(operation);
+            Assert.AreEqual(1, operation.Delta);
+            Assert.AreEqual(1, operation.Initial);
+            Assert.AreEqual("bucket", operation.BucketName);
+            Assert.AreEqual(0, operation.Expires);
+            Assert.AreEqual(10, operation.Timeout);
+        }
+
+        [Test]
+        public async Task IncrementAsync_With_Delta_ExecutesCorrectOperation()
+        {
+            // Arrange
+
+            Increment operation = null;
+
+            var mockRequestExecuter = new Mock<IRequestExecuter>();
+            mockRequestExecuter
+                .Setup(m => m.SendWithRetryAsync(It.IsAny<Increment>(), null, null))
+                .Callback((IOperation<ulong> op, TaskCompletionSource<IOperationResult<ulong>> tcs, CancellationTokenSource ccs) => operation = (Increment)op)
+                .ReturnsAsync((IOperationResult<ulong>)null);
+
+            // Act
+
+            using (var bucket = new CouchbaseBucket(mockRequestExecuter.Object, _converter, _transcoder))
+            {
+                bucket.Name = "bucket";
+
+                await bucket.IncrementAsync("key", 2);
+            }
+
+            // Assert
+
+            Assert.NotNull(operation);
+            Assert.AreEqual(2, operation.Delta);
+            Assert.AreEqual(1, operation.Initial);
+            Assert.AreEqual("bucket", operation.BucketName);
+            Assert.AreEqual(0, operation.Expires);
+            Assert.AreEqual(2, operation.Timeout);
+        }
+
+        [Test]
+        public async Task IncrementAsync_With_DeltaTimeout_ExecutesCorrectOperation()
+        {
+            // Arrange
+
+            Increment operation = null;
+
+            var mockRequestExecuter = new Mock<IRequestExecuter>();
+            mockRequestExecuter
+                .Setup(m => m.SendWithRetryAsync(It.IsAny<Increment>(), null, null))
+                .Callback((IOperation<ulong> op, TaskCompletionSource<IOperationResult<ulong>> tcs, CancellationTokenSource ccs) => operation = (Increment)op)
+                .ReturnsAsync((IOperationResult<ulong>)null);
+
+            // Act
+
+            using (var bucket = new CouchbaseBucket(mockRequestExecuter.Object, _converter, _transcoder))
+            {
+                bucket.Name = "bucket";
+
+                await bucket.IncrementAsync("key", 2, TimeSpan.FromSeconds(10));
+            }
+
+            // Assert
+
+            Assert.NotNull(operation);
+            Assert.AreEqual(2, operation.Delta);
+            Assert.AreEqual(1, operation.Initial);
+            Assert.AreEqual("bucket", operation.BucketName);
+            Assert.AreEqual(0, operation.Expires);
+            Assert.AreEqual(10, operation.Timeout);
+        }
+
+        [Test]
+        public async Task IncrementAsync_With_DeltaInitial_ExecutesCorrectOperation()
+        {
+            // Arrange
+
+            Increment operation = null;
+
+            var mockRequestExecuter = new Mock<IRequestExecuter>();
+            mockRequestExecuter
+                .Setup(m => m.SendWithRetryAsync(It.IsAny<Increment>(), null, null))
+                .Callback((IOperation<ulong> op, TaskCompletionSource<IOperationResult<ulong>> tcs, CancellationTokenSource ccs) => operation = (Increment)op)
+                .ReturnsAsync((IOperationResult<ulong>)null);
+
+            // Act
+
+            using (var bucket = new CouchbaseBucket(mockRequestExecuter.Object, _converter, _transcoder))
+            {
+                bucket.Name = "bucket";
+
+                await bucket.IncrementAsync("key", 2, 4);
+            }
+
+            // Assert
+
+            Assert.NotNull(operation);
+            Assert.AreEqual(2, operation.Delta);
+            Assert.AreEqual(4, operation.Initial);
+            Assert.AreEqual("bucket", operation.BucketName);
+            Assert.AreEqual(0, operation.Expires);
+            Assert.AreEqual(2, operation.Timeout);
+        }
+
+        [Test]
+        public async Task IncrementAsync_With_DeltaInitialExpirationTime_ExecutesCorrectOperation()
+        {
+            // Arrange
+
+            Increment operation = null;
+
+            var mockRequestExecuter = new Mock<IRequestExecuter>();
+            mockRequestExecuter
+                .Setup(m => m.SendWithRetryAsync(It.IsAny<Increment>(), null, null))
+                .Callback((IOperation<ulong> op, TaskCompletionSource<IOperationResult<ulong>> tcs, CancellationTokenSource ccs) => operation = (Increment)op)
+                .ReturnsAsync((IOperationResult<ulong>)null);
+
+            // Act
+
+            using (var bucket = new CouchbaseBucket(mockRequestExecuter.Object, _converter, _transcoder))
+            {
+                bucket.Name = "bucket";
+
+                await bucket.IncrementAsync("key", 2, 4, TimeSpan.FromSeconds(10));
+            }
+
+            // Assert
+
+            Assert.NotNull(operation);
+            Assert.AreEqual(2, operation.Delta);
+            Assert.AreEqual(4, operation.Initial);
+            Assert.AreEqual("bucket", operation.BucketName);
+            Assert.AreEqual(10, operation.Expires);
+            Assert.AreEqual(2, operation.Timeout);
+        }
+
+        [Test]
+        public async Task IncrementAsync_With_DeltaInitialExpiration_ExecutesCorrectOperation()
+        {
+            // Arrange
+
+            Increment operation = null;
+
+            var mockRequestExecuter = new Mock<IRequestExecuter>();
+            mockRequestExecuter
+                .Setup(m => m.SendWithRetryAsync(It.IsAny<Increment>(), null, null))
+                .Callback((IOperation<ulong> op, TaskCompletionSource<IOperationResult<ulong>> tcs, CancellationTokenSource ccs) => operation = (Increment)op)
+                .ReturnsAsync((IOperationResult<ulong>)null);
+
+            // Act
+
+            using (var bucket = new CouchbaseBucket(mockRequestExecuter.Object, _converter, _transcoder))
+            {
+                bucket.Name = "bucket";
+
+                await bucket.IncrementAsync("key", 2, 4, 10);
+            }
+
+            // Assert
+
+            Assert.NotNull(operation);
+            Assert.AreEqual(2, operation.Delta);
+            Assert.AreEqual(4, operation.Initial);
+            Assert.AreEqual("bucket", operation.BucketName);
+            Assert.AreEqual(10, operation.Expires);
+            Assert.AreEqual(2, operation.Timeout);
+        }
+
+        [Test]
+        public async Task IncrementAsync_With_DeltaInitialExpirationTimeTimeout_ExecutesCorrectOperation()
+        {
+            // Arrange
+
+            Increment operation = null;
+
+            var mockRequestExecuter = new Mock<IRequestExecuter>();
+            mockRequestExecuter
+                .Setup(m => m.SendWithRetryAsync(It.IsAny<Increment>(), null, null))
+                .Callback((IOperation<ulong> op, TaskCompletionSource<IOperationResult<ulong>> tcs, CancellationTokenSource ccs) => operation = (Increment)op)
+                .ReturnsAsync((IOperationResult<ulong>)null);
+
+            // Act
+
+            using (var bucket = new CouchbaseBucket(mockRequestExecuter.Object, _converter, _transcoder))
+            {
+                bucket.Name = "bucket";
+
+                await bucket.IncrementAsync("key", 2, 4, TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(20));
+            }
+
+            // Assert
+
+            Assert.NotNull(operation);
+            Assert.AreEqual(2, operation.Delta);
+            Assert.AreEqual(4, operation.Initial);
+            Assert.AreEqual("bucket", operation.BucketName);
+            Assert.AreEqual(10, operation.Expires);
+            Assert.AreEqual(20, operation.Timeout);
+        }
+
+        [Test]
+        public async Task IncrementAsync_With_DeltaInitialExpirationTimeout_ExecutesCorrectOperation()
+        {
+            // Arrange
+
+            Increment operation = null;
+
+            var mockRequestExecuter = new Mock<IRequestExecuter>();
+            mockRequestExecuter
+                .Setup(m => m.SendWithRetryAsync(It.IsAny<Increment>(), null, null))
+                .Callback((IOperation<ulong> op, TaskCompletionSource<IOperationResult<ulong>> tcs, CancellationTokenSource ccs) => operation = (Increment)op)
+                .ReturnsAsync((IOperationResult<ulong>)null);
+
+            // Act
+
+            using (var bucket = new CouchbaseBucket(mockRequestExecuter.Object, _converter, _transcoder))
+            {
+                bucket.Name = "bucket";
+
+                await bucket.IncrementAsync("key", 2, 4, 10, TimeSpan.FromSeconds(20));
+            }
+
+            // Assert
+
+            Assert.NotNull(operation);
+            Assert.AreEqual(2, operation.Delta);
+            Assert.AreEqual(4, operation.Initial);
+            Assert.AreEqual("bucket", operation.BucketName);
+            Assert.AreEqual(10, operation.Expires);
+            Assert.AreEqual(20, operation.Timeout);
+        }
+    }
+}
