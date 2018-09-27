080ce71b4e97980ea70db3cba00e7ea9acc3aaa3
Mike Goldsmith <goldsmith.mike@gmail.com>
2018-09-27 09:51:23 +0000

    Revert "NCBC-1771: Fix intermitent Test_MultiGet on CI server"
    This reverts commit b110edc800c380d1759278c3db0e0b0b03b030f7.
    
    Reason for revert: Has caused the CI builds to become unstable. Requires further investigate. Also, Using Parallel.ForEach is not a supported method to execute parallel async operations - tasks should be used instead.
    
    Change-Id: Ib2de309b77f37c048f673a46d3a7a018b151c4b1
    Reviewed-on: http://review.couchbase.org/100025
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Charles Dixon <chvckd@gmail.com>

--- Src/Couchbase.IntegrationTests/CouchbaseBucket_Ssl_Tests.cs
+++ Src/Couchbase.IntegrationTests/CouchbaseBucket_Ssl_Tests.cs
@@ -90,32 +90,11 @@ namespace Couchbase.IntegrationTests
 
             Parallel.ForEach(keys, key =>
             {
-                var upsertResult = _bucket.Upsert(key, value);
-                Assert.IsTrue(upsertResult.Success, upsertResult.Message);
+                _bucket.Upsert(key, value);
 
-                var getResult = _bucket.Get<string>(key);
-                Assert.IsTrue(getResult.Success, getResult.Message);
-                Assert.AreEqual(ResponseStatus.Success, getResult.Status);
-            });
-        }
+                var result = _bucket.Get<string>(key);
 
-        [Test]
-        public void Test_MultiGetAsync()
-        {
-            // This test helps to ensure that the SSL connections are performing well under load
-            // and there are no buffer overlap issues when multiple commands are issued via an SslConnection
-
-            var keys = Enumerable.Range(0, 100).Select(p => "thekey" + p);
-            var value = "thevalue";
-
-            Parallel.ForEach(keys, async key =>
-            {
-                var upsertResult = await _bucket.UpsertAsync(key, value);
-                Assert.IsTrue(upsertResult.Success, upsertResult.Message);
-
-                var getResult = await _bucket.GetAsync<string>(key);
-                Assert.IsTrue(getResult.Success, getResult.Message);
-                Assert.AreEqual(ResponseStatus.Success, getResult.Status);
+                Assert.AreEqual(ResponseStatus.Success, result.Status);
             });
         }
 
