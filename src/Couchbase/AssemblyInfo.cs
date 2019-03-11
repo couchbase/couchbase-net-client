145570fc1e022be84c4d74a0f0d6037f6660dec8
Mike Goldsmith <goldsmith.mike@gmail.com>
2019-03-11 17:28:09 +0000

    NCBC-1861: Only use InternalsVisibleTo for test projects in Debug mode
    MOTIVATION
    ----------
    The SDK needs to be signed correctly when creating a nuget package.
    However the Couchbase.UnitTests project uses an IntervalsVisibleTo
    reference that is not valid. This is only needed at debug time.
    
    MODIFICATIONS
    -------------
    - add DEBUG and RELEASE constants to csproj
    - use build flag to only include IntervalsVisibleTo for
      Couchbase.UnitTests when in Release mode
    
    RESULT
    ------
    The Couchbase.UnitTest can only see Couchbase SDK internals when in
    Debug mode.
    
    Change-Id: I3a821aa78b33f40e72ed17566e831aa284129e5b
    Reviewed-on: http://review.couchbase.org/106008
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Jeffry Morris <jeffrymorris@gmail.com>

--- src/Couchbase/AssemblyInfo.cs
+++ src/Couchbase/AssemblyInfo.cs
@@ -1,8 +1,8 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyDescription("")]
@@ -10,12 +10,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a877a9c8-b003-44de-bd70-8bb90b1b9f9e")]
 
+#if DEBUG
 [assembly: InternalsVisibleTo("Couchbase.UnitTests")]
+#endif
