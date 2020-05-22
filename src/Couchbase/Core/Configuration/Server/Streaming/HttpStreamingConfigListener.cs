4c587ccd5bf2e4b6162aec5d0c27c77561ad4190
RiPont <richard.ponton@couchbase.com>
2020-05-22 12:07:54 -0700

    NCBC-2518: Make HttpStreamingConfigListener more resiliant to failures.
    Motivation
    ----------
    Currently, it would exit silently in the background if the streams went down.
    
    Modifications
    -------------
    - Outer loop to restart from the beginning if the inner loop exited.
    - If the background task somehow stopped running, it will be restarted the next time a memcachedbucket is registered and StartListening() is called.
    - Unit Test to confirm the new resiliance.
    
    Results
    -------
    - HttpStreamingConfigListener now runs continuously, even if all the initial streams fail.
    
    Change-Id: I96494c839ee87db7085f77c4db34909de3632168
    Reviewed-on: http://review.couchbase.org/c/couchbase-net-client/+/128882
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Jeffry Morris <jeffrymorris@gmail.com>

--- src/Couchbase/Core/Configuration/Server/Streaming/HttpStreamingConfigListener.cs
+++ src/Couchbase/Core/Configuration/Server/Streaming/HttpStreamingConfigListener.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
@@ -14,14 +15,19 @@ using Newtonsoft.Json;
 
 namespace Couchbase.Core.Configuration.Server.Streaming
 {
-    internal class HttpStreamingConfigListener : IDisposable
+    internal class HttpStreamingConfigListener : IDisposable, IAsyncDisposable
     {
+        private const int InitialDelayMs = 0;
+        private const int MaxDelayMs = 10000;
         private readonly ILogger<HttpStreamingConfigListener> _logger;
         private readonly ClusterOptions _clusterOptions;
         private readonly HttpClient _httpClient;
         private readonly IConfigHandler _configHandler;
         private readonly string _bucketName;
+        private readonly string _streamingUriPath;
         private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        private Task? _backgroundTask = null;
+        private readonly object _manageTaskLock = new object();
 
         private bool _disposed;
 
@@ -31,6 +37,7 @@ namespace Couchbase.Core.Configuration.Server.Streaming
             IConfigHandler configHandler, ILogger<HttpStreamingConfigListener> logger)
         {
             _bucketName = bucketName ?? throw new ArgumentNullException(nameof(bucketName));
+            _streamingUriPath = "/pools/default/bs/" + _bucketName;
             _clusterOptions = clusterOptions ?? throw new ArgumentNullException(nameof(clusterOptions));
             _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
             _configHandler = configHandler ?? throw new ArgumentNullException(nameof(configHandler));
@@ -44,68 +51,99 @@ namespace Couchbase.Core.Configuration.Server.Streaming
                 throw new ObjectDisposedException(nameof(HttpStreamingConfigListener));
             }
 
-            if (Started)
+            if (Started && _backgroundTask?.Status.HasFlag(TaskStatus.Running) == true)
             {
                 return;
             }
 
             Started = true;
 
-            var streamingUriPath = "/pools/default/bs/" + _bucketName;
+            lock (_manageTaskLock)
+            {
+                if (_backgroundTask?.Status.HasFlag(TaskStatus.Running) != true)
+                {
+                    _backgroundTask = StartBackgroundTask();
+                }
+            }
+        }
 
-            Task.Run(async () =>
+        private Task StartBackgroundTask()
+        {
+            return Task.Run(async () =>
             {
                 _httpClient.Timeout = Timeout.InfiniteTimeSpan;
+                var delayMs = InitialDelayMs;
 
-                var servers = _clusterOptions.ConnectionStringValue?.GetBootstrapEndpoints().ToList().Shuffle();
-
-                while (servers.Any())
+                while (!_cancellationTokenSource.IsCancellationRequested)
                 {
                     try
                     {
-                        var server = servers.First();
-                        servers?.Remove(server);
 
-                        var streamingUri = new UriBuilder()
+                        var servers = _clusterOptions.ConnectionStringValue?.GetBootstrapEndpoints().ToList().Shuffle();
+
+                        while (servers.Any())
                         {
-                            Scheme = _clusterOptions.EffectiveEnableTls ? Uri.UriSchemeHttps : Uri.UriSchemeHttp,
-                            Host = server.Host,
-                            Port = _clusterOptions.BootstrapHttpPort,
-                            Path = streamingUriPath
-                        };
-
-                        var response = await _httpClient.GetAsync(streamingUri.Uri,
-                            HttpCompletionOption.ResponseHeadersRead,
-                            _cancellationTokenSource.Token).ConfigureAwait(false);
-
-                        response.EnsureSuccessStatusCode();
-
-                        using var stream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
-                        if (stream.CanTimeout)
-                        {
-                            //the stream itself can timeout if CanTimeout is true on a platform
-                            stream.ReadTimeout = Timeout.Infinite;
-                        }
-
-                        using var reader = new StreamReader(stream, Encoding.UTF8, false);
-
-                        string? config;
-                        while (!_cancellationTokenSource.IsCancellationRequested &&
-                               (config = await reader.ReadLineAsync().ConfigureAwait(false)) != null)
-                        {
-                            if (config != string.Empty)
+                            try
                             {
-                                _logger.LogDebug(LoggingEvents.ConfigEvent, config);
-                                config = config.Replace("$HOST", server.Host);
-                                var bucketConfig = JsonConvert.DeserializeObject<BucketConfig>(config);
-                                _configHandler.Publish(bucketConfig);
+                                var server = servers.First();
+                                servers?.Remove(server);
+
+                                var streamingUri = new UriBuilder()
+                                {
+                                    Scheme =
+                                        _clusterOptions.EffectiveEnableTls ? Uri.UriSchemeHttps : Uri.UriSchemeHttp,
+                                    Host = server.Host,
+                                    Port = _clusterOptions.BootstrapHttpPort,
+                                    Path = _streamingUriPath
+                                };
+
+                                var response = await _httpClient.GetAsync(streamingUri.Uri,
+                                    HttpCompletionOption.ResponseHeadersRead,
+                                    _cancellationTokenSource.Token).ConfigureAwait(false);
+
+                                response.EnsureSuccessStatusCode();
+
+                                using var stream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+                                if (stream.CanTimeout)
+                                {
+                                    //the stream itself can timeout if CanTimeout is true on a platform
+                                    stream.ReadTimeout = Timeout.Infinite;
+                                }
+
+                                using var reader = new StreamReader(stream, Encoding.UTF8, false);
+
+                                string? config;
+                                while (!_cancellationTokenSource.IsCancellationRequested &&
+                                       (config = await reader.ReadLineAsync().ConfigureAwait(false)) != null)
+                                {
+                                    if (config != string.Empty)
+                                    {
+                                        _logger.LogDebug(LoggingEvents.ConfigEvent, config);
+                                        config = config.Replace("$HOST", server.Host);
+                                        var bucketConfig = JsonConvert.DeserializeObject<BucketConfig>(config);
+                                        _configHandler.Publish(bucketConfig);
+                                    }
+
+                                    // on success, reset the exponential delay
+                                    delayMs = InitialDelayMs;
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                _logger.LogError(e, "HTTP Streaming error.");
                             }
                         }
                     }
                     catch (Exception e)
                     {
-                        _logger.LogError(e, "HTTP Streaming error.");
+                        _logger.LogError(e, "HTTP Streaming error. (outer loop)");
                     }
+
+                    // if we exited the inner loop, then all servers failed and we need to start over.
+                    // however, we don't want to create a failstorm in the logs if the failure is 100%
+                    // try again, but with an exponential delay of up to 10s.
+                    await Task.Delay(delayMs).ConfigureAwait(false);
+                    delayMs = Math.Min(delayMs * 10, MaxDelayMs);
                 }
             }, _cancellationTokenSource.Token);
         }
@@ -119,6 +157,15 @@ namespace Couchbase.Core.Configuration.Server.Streaming
                 _cancellationTokenSource.Cancel();
                 _cancellationTokenSource.Dispose();
                 _httpClient?.Dispose();
+            }
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            Dispose();
+            if (_backgroundTask != null)
+            {
+                await _backgroundTask;
             }
         }
     }
---
+++ tests/Couchbase.UnitTests/Core/Configuration/HttpStreamingConfigListenerTests.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Couchbase.Core;
+using Couchbase.Core.Bootstrapping;
+using Couchbase.Core.Configuration.Server;
+using Couchbase.Core.Configuration.Server.Streaming;
+using Couchbase.Core.DI;
+using Couchbase.Core.IO.Operations;
+using Couchbase.Core.Logging;
+using Couchbase.Core.Retry;
+using Couchbase.KeyValue;
+using Couchbase.Management.Collections;
+using Couchbase.Management.Views;
+using Couchbase.Views;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Couchbase.UnitTests.Core.Configuration
+{
+    public class HttpStreamingConfigListenerTests
+    {
+        [Fact]
+        public async Task Should_Continue_After_Failures()
+        {
+            var clusterOptions = new ClusterOptions()
+                .WithConnectionString($"couchbases://NOSUCHHOST{nameof(Should_Continue_After_Failures)}")
+                .WithCredentials("UnitTestUser", "PasswordDoesn'tMatter");
+            var messageHandler = new ThrowsEveryTimeMessageHandler();
+            var httpClient = new HttpClient(messageHandler);
+            var configHandler = new Mock<IConfigHandler>(MockBehavior.Loose).Object;
+            var mockLogger = new Mock<ILogger<HttpStreamingConfigListener>>(MockBehavior.Loose).Object;
+            using var configListener = new HttpStreamingConfigListener(nameof(Should_Continue_After_Failures),
+                clusterOptions, httpClient, configHandler, mockLogger);
+            configListener.StartListening();
+            await Task.Delay(500);
+            Assert.NotInRange(messageHandler.CallCount, 0, 2);
+            configListener.Dispose();
+
+            // give it a little time to finish up.
+            await Task.Delay(500);
+            var callCountAfterDispose = messageHandler.CallCount;
+
+            // a little more time to generate more calls if it was still running.
+            await Task.Delay(500);
+            Assert.Equal(callCountAfterDispose, messageHandler.CallCount);
+
+            await configListener.DisposeAsync();
+        }
+
+        class ThrowsEveryTimeMessageHandler : HttpMessageHandler
+        {
+            public int CallCount { get; private set; }
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                CallCount++;
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
