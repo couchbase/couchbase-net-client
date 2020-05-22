6c24280d500d1d55f5cdb74322bfd2a75c5253dd
Jeff Morris <jeffry.morris@couchbase.com>
2020-05-21 21:05:21 -0700

    NCBC-2517: DependencyInjection project refactoring
    Motivation
    ----------
    Add a SIGNING constant to compile out code not required for signing and
    updated the package property values for license, project url, etc.
    
    Change-Id: I4d270da99b77e1c2d0379cb5706b1b1e86a91d83
    Reviewed-on: http://review.couchbase.org/c/couchbase-net-client/+/128812
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: <richard.ponton@couchbase.com>

--- src/Couchbase.Extensions.DependencyInjection/Properties/AssemblyInfo.cs
+++ src/Couchbase.Extensions.DependencyInjection/Properties/AssemblyInfo.cs
@@ -1,5 +1,7 @@
 using System.Runtime.CompilerServices;
 
+#if !SIGNING
 [assembly: InternalsVisibleTo("Couchbase.Extensions.DependencyInjection.Dynamic")]
 [assembly: InternalsVisibleTo("Couchbase.Extensions.DependencyInjection.UnitTests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+#endif
