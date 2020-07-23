f6f6a89e257adce1251b2b9a4e7836c5932ed539
Jeff Morris <jeffry.morris@couchbase.com>
2020-07-22 18:32:07 -0700

    NCBC-2609: Add PublicKey to AssemblyInfo for DI when building release packages
    Motivation
    ----------
    There are some friend assemblies which break when signing a release
    build. These can be fixed by adding the PublicKey to the
    InternalsVisibleTo declarations on release builds that are signed.
    
    Modifications
    -------------
     - Add PublicKey to InternalVisibleTo attributes when strong name siging
     builds
    
    Result
    ------
    When sigining is enabled it will succeed because the PublicKey of the
    siging key is available.
    
    Change-Id: Ia78bc5a8f4defa75f28f95acd3b08e08601dfdc5
    Reviewed-on: http://review.couchbase.org/c/couchbase-net-client/+/133107
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: <richard.ponton@couchbase.com>

--- src/Couchbase.Extensions.DependencyInjection/Properties/AssemblyInfo.cs
+++ src/Couchbase.Extensions.DependencyInjection/Properties/AssemblyInfo.cs
@@ -1,8 +1,13 @@
 using System.Runtime.CompilerServices;
 
+#if SIGNING
 // These internals must be exposed in production builds to support dynamic proxy generation
+[assembly: InternalsVisibleTo("Couchbase.Extensions.DependencyInjection.Dynamic, PublicKey=05e9c6b5a9ec94c2")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=05e9c6b5a9ec94c2")]
+#else
 [assembly: InternalsVisibleTo("Couchbase.Extensions.DependencyInjection.Dynamic")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+#endif
 
 #if !SIGNING
 [assembly: InternalsVisibleTo("Couchbase.Extensions.DependencyInjection.UnitTests")]
