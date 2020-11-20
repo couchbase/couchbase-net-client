09121b014c4561d71d335507a0034a54ec0f2aa6
Nouphal Kadavath Purayil <nouphal@couchbase.com>
2020-11-20 15:32:52 -0800

    NCBC-2716:Collection Analytics Test changes
    Change-Id: I69ddfa8a03aca8c4a03559b2bad7b0904888d5a0
    Reviewed-on: http://review.couchbase.org/c/couchbase-net-client/+/140750
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Jeffry Morris <jeffrymorris@gmail.com>

--- tests/Couchbase.IntegrationTests/Services/Analytics/AnalyticsTests.cs
+++ tests/Couchbase.IntegrationTests/Services/Analytics/AnalyticsTests.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Couchbase.Analytics;
 using Couchbase.IntegrationTests.Fixtures;
+using Couchbase.IntegrationTests.Utils;
+using Couchbase.Management.Analytics;
+using Couchbase.Management.Collections;
 using Couchbase.Query;
 using Moq;
 using Newtonsoft.Json;
@@ -14,6 +18,31 @@ namespace Couchbase.IntegrationTests.Services.Analytics
     public class AnalyticsTests : IClassFixture<ClusterFixture>
     {
         private readonly ClusterFixture _fixture;
+        private static AnalyticsIndexManager analytics;
+        private static string scopeName = "myScope" + randomString();
+        private static string collectionName = "myCollection" + randomString();
+
+        private static string randomString()
+        {
+            {
+                int length = 7;
+
+                // creating a StringBuilder object()
+                StringBuilder str_build = new StringBuilder();
+                Random random = new Random();
+
+                char letter;
+
+                for (int i = 0; i < length; i++)
+                {
+                    double flt = random.NextDouble();
+                    int shift = Convert.ToInt32(Math.Floor(25 * flt));
+                    letter = Convert.ToChar(shift + 65);
+                    str_build.Append(letter);
+                }
+                return str_build.ToString();
+            }
+        }
 
         public AnalyticsTests(ClusterFixture fixture)
         {
@@ -56,6 +85,39 @@ namespace Couchbase.IntegrationTests.Services.Analytics
             ).ConfigureAwait(false);
 
             Assert.True(result.Any());
+        }
+
+        [CouchbaseVersionDependentFact(MinVersion = "7.0.0")]
+        public async Task Test_Collections_DataverseCollectionQuery()
+        {
+            var cluster = await _fixture.GetCluster().ConfigureAwait(false);
+            var bucket = await _fixture.Cluster.BucketAsync("default").ConfigureAwait(false);
+            string dataverseName = bucket.Name + "." + scopeName;
+            var collectionManager = (CollectionManager)bucket.Collections;
+            var scopeSpec = new ScopeSpec(scopeName);
+            await analytics.CreateDataverseAsync(dataverseName);
+            string statement = "CREATE ANALYTICS COLLECTION `" + dataverseName + "`.`" + collectionName + "` ON `" + bucket.Name + "`.`" + scopeName + "`.`" + collectionName + "`";
+
+            try
+            {
+                var result = await cluster.AnalyticsQuery<TestRequest>(statement)
+               .ToListAsync().ConfigureAwait(false);
+                await collectionManager.CreateScopeAsync(scopeSpec).ConfigureAwait(false);
+                var collectionSpec = new CollectionSpec(scopeName, collectionName);
+                await collectionManager.CreateCollectionAsync(collectionSpec).ConfigureAwait(false);
+                var collectionExistsResult = await collectionManager.CollectionExistsAsync(collectionSpec).ConfigureAwait(false);
+                Assert.True(collectionExistsResult);
+                var scope = bucket.Scope(scopeName);
+                statement = "SELECT * FROM `" + collectionName + "` where `" + collectionName + "`.foo= \"bar\"";
+                result = await cluster.AnalyticsQuery<TestRequest>(statement).ToListAsync().ConfigureAwait(false);
+                Assert.True(result.Any());
+
+            }
+            finally
+            {
+                // drop scope
+                await collectionManager.DropScopeAsync(scopeName).ConfigureAwait(false);
+            }
         }
     }
 }
