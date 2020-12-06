c6516e54c6c5ceee98a2586754e473508f7f1bb5
Brant Burnett <bburnett@centeredgesoftware.com>
2020-12-06 14:27:34 -0500

    NCBC-2737: Use a static client description for spans
    Motivation
    ----------
    Avoid CPU cycles and heap allocations building a constant string
    repeatedly for every query or key/value operation.
    
    Modifications
    -------------
    Build the client description once and reuse for the lifetime of the
    application, since all of its components are static.
    
    Drop duplicate GetRandomLong method, reusing the one from
    SequenceGenerator.
    
    Results
    -------
    Reduced CPU and heap allocations, especially valuable when building
    tracing spans for key/value operations.
    
    Change-Id: I38181ef4454919539ec8dd6c292bcaa720c754b7
    Reviewed-on: http://review.couchbase.org/c/couchbase-net-client/+/141560
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: <richard.ponton@couchbase.com>

--- src/Couchbase/Utils/ClientIdentifier.cs
+++ src/Couchbase/Utils/ClientIdentifier.cs
@@ -1,35 +1,24 @@
-using System;
 using System.Runtime.InteropServices;
+using Couchbase.Core.IO.Operations;
 
 namespace Couchbase.Utils
 {
     internal static class ClientIdentifier
     {
-        private static readonly Random Random = new Random();
         private const string DescriptionFormat = "couchbase-net-sdk/{0} (clr/{1}) (os/{2})";
 
-        internal static ulong InstanceId = GetRandomLong();
+        private static readonly string ClientDescription =
+            string.Format(DescriptionFormat, CurrentAssembly.Version, RuntimeInformation.FrameworkDescription,
+                RuntimeInformation.OSDescription);
 
-        public static string GetClientDescription()
-        {
-            return string.Format(DescriptionFormat, CurrentAssembly.Version, RuntimeInformation.FrameworkDescription, RuntimeInformation.OSDescription);
-        }
+        internal static ulong InstanceId = SequenceGenerator.GetRandomLong();
+
+        public static string GetClientDescription() => ClientDescription;
 
         public static string FormatConnectionString(ulong connectionId)
         {
             // format as hex padded to 16 spaces
             return $"{InstanceId:x16}/{connectionId:x16}";
-        }
-
-        public static ulong GetRandomLong()
-        {
-            var bytes = new byte[8];
-            lock (Random)
-            {
-                Random.NextBytes(bytes);
-            }
-
-            return BitConverter.ToUInt64(bytes, 0);
         }
     }
 }
