6e6e008c4bfdf8df942d616b9d9e4726b82e3dd3
RiPont <richard.ponton@couchbase.com>
2022-01-25 13:35:05 -0800

    Update version post-release.
    Tweak OpenTelemetry csproj to allow publishing.
    
    Change-Id: Iae51b716e70cfabc29024b81911b71d419550f9c
    Reviewed-on: https://review.couchbase.org/c/couchbase-net-client/+/169460
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Jeffry Morris <jeffrymorris@gmail.com>

--- src/Couchbase.Extensions.OpenTelemetry/Properties/AssemblyInfo.cs
+++ src/Couchbase.Extensions.OpenTelemetry/Properties/AssemblyInfo.cs
@@ -1,3 +1,8 @@
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
+[assembly: ComVisible(false)]
+
+#if !SIGNING
 [assembly: InternalsVisibleTo("Couchbase.Extensions.OpenTelemetry.UnitTests")]
+#endif
