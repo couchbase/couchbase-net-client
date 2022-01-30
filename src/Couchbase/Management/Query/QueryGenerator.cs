1799e2c9503d713572bed1844e2bef99a030906f
Jeff Morris <jeffry.morris@couchbase.com>
2022-01-29 19:02:19 -0800

    NCBC-3107: Escape keyspace values with back ticks if missing
    Motivation
    ----------
    Bucket, scope and collections require backtics when creating the
    statement internally. This commit adds them if they are missing.
    
    Modifications
    -------------
     - Escape keyspace values with backtics if missing in QueryGenerator
     - Update unit tests
    
    Change-Id: I19457b3ac69d2d94f728b22414b219f2c6fe5239
    Reviewed-on: https://review.couchbase.org/c/couchbase-net-client/+/169706
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Brant Burnett <bburnett@centeredgesoftware.com>
    Reviewed-by: Richard Ponton <richard.ponton@couchbase.com>

--- src/Couchbase/Management/Query/QueryGenerator.cs
+++ src/Couchbase/Management/Query/QueryGenerator.cs
@@ -1,3 +1,4 @@
+using Couchbase.Core.Utils;
 using System.Collections.Generic;
 
 namespace Couchbase.Management.Query
@@ -8,54 +9,54 @@ namespace Couchbase.Management.Query
         {
             if(options.ScopeNameValue == null)
             {
-                return $"CREATE INDEX `{indexName}` ON `{bucketName}`({string.Join(",", fields)}) USING GSI WITH {{\"defer_build\":{options.DeferredValue}}};";
+                return $"CREATE INDEX {indexName.EscapeIfRequired()} ON {bucketName.EscapeIfRequired()}({string.Join(",", fields)}) USING GSI WITH {{\"defer_build\":{options.DeferredValue}}};";
             }
-            return $"CREATE INDEX `{indexName}` ON `{bucketName}`.`{options.ScopeNameValue}`.`{options.CollectionNameValue}`({string.Join(",", fields)}) USING GSI WITH {{\"defer_build\":{options.DeferredValue}}};";
+            return $"CREATE INDEX {indexName.EscapeIfRequired()} ON {bucketName.EscapeIfRequired()}.{options.ScopeNameValue.EscapeIfRequired()}.{options.CollectionNameValue.EscapeIfRequired()}({string.Join(",", fields)}) USING GSI WITH {{\"defer_build\":{options.DeferredValue}}};";
         }
 
         public static string CreateDeferredIndexStatement(string bucketName, string indexName, BuildDeferredQueryIndexOptions options)
         {
             if(options.ScopeNameValue == null)
             {
-                return $"BUILD INDEX ON `{bucketName}`({indexName}) USING GSI;";
+                return $"BUILD INDEX ON {bucketName.EscapeIfRequired()}({indexName}) USING GSI;";
             }
-            return $"BUILD INDEX ON `{bucketName}`.`{options.ScopeNameValue}`.`{options.CollectionNameValue}`({indexName}) USING GSI;";
+            return $"BUILD INDEX ON {bucketName.EscapeIfRequired()}.{options.ScopeNameValue.EscapeIfRequired()}.{options.CollectionNameValue.EscapeIfRequired()}({indexName}) USING GSI;";
         }
 
         public static string CreatePrimaryIndexStatement(string bucketName, CreatePrimaryQueryIndexOptions options = null)
         {
             if (options.ScopeNameValue == null)
             {
-                return $"CREATE PRIMARY INDEX ON `{bucketName}` USING GSI WITH {{\"defer_build\":{options.DeferredValue}}};";
+                return $"CREATE PRIMARY INDEX ON {bucketName.EscapeIfRequired()} USING GSI WITH {{\"defer_build\":{options.DeferredValue}}};";
             }
-            return $"CREATE PRIMARY INDEX ON `{bucketName}`.`{options.ScopeNameValue}`.`{options.CollectionNameValue}` USING GSI WITH {{\"defer_build\":{options.DeferredValue}}};";
+            return $"CREATE PRIMARY INDEX ON {bucketName.EscapeIfRequired()}.{options.ScopeNameValue.EscapeIfRequired()}.{options.CollectionNameValue.EscapeIfRequired()} USING GSI WITH {{\"defer_build\":{options.DeferredValue}}};";
         }
 
         public static string CreateDropIndexStatement(string bucketName, string indexName, DropQueryIndexOptions options)
         {
             if (options.ScopeNameValue == null)
             {
-                return $"DROP INDEX `{bucketName}`.`{indexName}` USING GSI;";
+                return $"DROP INDEX {bucketName.EscapeIfRequired()}.{indexName.EscapeIfRequired()} USING GSI;";
             }
-            return $"DROP INDEX `{indexName}` ON `{bucketName}`.`{options.ScopeNameValue}`.`{options.CollectionNameValue}` USING GSI;";
+            return $"DROP INDEX {indexName.EscapeIfRequired()} ON {bucketName.EscapeIfRequired()}.{options.ScopeNameValue.EscapeIfRequired()}.{options.CollectionNameValue.EscapeIfRequired()} USING GSI;";
         }
 
         public static string CreateDropPrimaryIndexStatement(string bucketName, DropPrimaryQueryIndexOptions options)
         {
             if (options.ScopeNameValue == null)
             {
-                return $"DROP PRIMARY INDEX ON `{bucketName}` USING GSI;";
+                return $"DROP PRIMARY INDEX ON {bucketName.EscapeIfRequired()} USING GSI;";
             }
-            return $"DROP PRIMARY INDEX ON `{bucketName}`.`{options.ScopeNameValue}`.`{options.CollectionNameValue}` USING GSI;";
+            return $"DROP PRIMARY INDEX ON {bucketName.EscapeIfRequired()}.{options.ScopeNameValue.EscapeIfRequired()}.{options.CollectionNameValue.EscapeIfRequired()} USING GSI;";
         }
 
         public static string CreateGetAllIndexesStatement(string bucketName, GetAllQueryIndexOptions options)
         {
             if(options.ScopeNameValue == null)
             {
-                return $"SELECT i.* FROM system:indexes AS i WHERE i.keyspace_id=`{bucketName}` AND `using`=\"gsi\";";
+                return $"SELECT i.* FROM system:indexes AS i WHERE i.keyspace_id={bucketName.EscapeIfRequired()} AND `using`=\"gsi\";";
             }
-            return $"SELECT i.* FROM system:indexes AS i WHERE i.bucket_id=`{bucketName}` AND scope_id=`{options.ScopeNameValue}` AND i.keyspace_id=`{options.CollectionNameValue}` AND `using`=\"gsi\";";
+            return $"SELECT i.* FROM system:indexes AS i WHERE i.bucket_id={bucketName.EscapeIfRequired()} AND scope_id={options.ScopeNameValue.EscapeIfRequired()} AND i.keyspace_id={options.CollectionNameValue.EscapeIfRequired()} AND `using`=\"gsi\";";
         }
     }
 }
