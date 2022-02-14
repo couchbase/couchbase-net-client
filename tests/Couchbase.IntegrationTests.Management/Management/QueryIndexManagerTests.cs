032fad82124ce71e76d7c76b4b9d55c340880b66
Will Broadbelt <wbroadbelt@gmail.com>
2022-02-14 14:24:29 +0000

    SDKQE-2431: Add testing for QueryIndexManager with collections
    Modifications:
    - Added QueryIndexManager tests that exercise the scope and collections
      options on all the QueryIndexManager functions
    - Also added a check for GetAllIndexesAsync to the original/non-collections
      tests for full coverage
    
    Change-Id: I45b8382c4a670c7fb12c3ac72066a4887ab79b5b
    Reviewed-on: https://review.couchbase.org/c/couchbase-net-client/+/170670
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Jeffry Morris <jeffrymorris@gmail.com>

--- tests/Couchbase.IntegrationTests.Management/Management/QueryIndexManagerTests.cs
+++ tests/Couchbase.IntegrationTests.Management/Management/QueryIndexManagerTests.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Couchbase.Core.Exceptions;
 using Couchbase.IntegrationTests.Fixtures;
+using Couchbase.IntegrationTests.Utils;
 using Couchbase.Management.Buckets;
+using Couchbase.Management.Collections;
 using Couchbase.Management.Query;
 using Xunit;
 using Xunit.Abstractions;
@@ -26,12 +29,13 @@ namespace Couchbase.IntegrationTests.Management
         public async Task CreateAndDropIndex()
         {
             var cluster = await _fixture.GetCluster().ConfigureAwait(false);
+            var bucketName = _fixture.GetDefaultBucket().Result.Name;
 
-            var indexName = "indexmgr_test";
+            const string indexName = "indexmgr_test";
             try
             {
                 await cluster.QueryIndexes.CreateIndexAsync(
-                    "default", indexName, new[] { "type" }).ConfigureAwait(false);
+                    bucketName, indexName, new[] { "type" }).ConfigureAwait(false);
             }
             catch (IndexExistsException)
             {
@@ -41,18 +45,21 @@ namespace Couchbase.IntegrationTests.Management
             bool failedCleanup = false;
             try
             {
-                await cluster.QueryIndexes.BuildDeferredIndexesAsync("default").ConfigureAwait(false);
+                await cluster.QueryIndexes.BuildDeferredIndexesAsync(bucketName).ConfigureAwait(false);
 
                 using var cts = new CancellationTokenSource(10000);
 
-                await cluster.QueryIndexes.WatchIndexesAsync("default", new[] {"indexmgr_test"},
+                await cluster.QueryIndexes.WatchIndexesAsync(bucketName, new[] {indexName},
                     options => { options.CancellationToken(cts.Token); }).ConfigureAwait(false);
+
+                var getIndexes = await cluster.QueryIndexes.GetAllIndexesAsync(bucketName);
+                Assert.Contains(indexName, getIndexes.Select(idx => idx.Name));
             }
             finally
             {
                 try
                 {
-                    await cluster.QueryIndexes.DropIndexAsync("default", "indexmgr_test").ConfigureAwait(false);
+                    await cluster.QueryIndexes.DropIndexAsync(bucketName, indexName).ConfigureAwait(false);
                 }
                 catch (Exception e)
                 {
@@ -62,6 +69,88 @@ namespace Couchbase.IntegrationTests.Management
             }
 
             Assert.False(failedCleanup);
+        }
+
+        [CouchbaseVersionDependentFact(MinVersion = "7.0.0")]
+        public async Task CreateAndDropCollectionIndex()
+        {
+            var cluster = await _fixture.GetCluster().ConfigureAwait(false);
+            var bucketName = _fixture.GetDefaultBucket().Result.Name;
+            var collectionManager = _fixture.GetDefaultBucket().Result.Collections;
+
+            var scopeName = Guid.NewGuid().ToString();
+            var collectionName = Guid.NewGuid().ToString();
+            var collectionSpec = new CollectionSpec(scopeName, collectionName);
+
+            try
+            {
+                await collectionManager.CreateScopeAsync(scopeName);
+                await collectionManager.CreateCollectionAsync(collectionSpec);
+
+                const string indexName = "indexmgr_test_collection";
+                try
+                {
+                    await cluster.QueryIndexes.CreateIndexAsync(bucketName, indexName, new[] {"type"}, options =>
+                        {
+                            options.ScopeNameValue = scopeName;
+                            options.CollectionNameValue = collectionName;
+                        }).ConfigureAwait(false);
+                }
+                catch (IndexExistsException)
+                {
+                    _outputHelper.WriteLine("IndexExistsException.  Maybe from a previous run.  Skipping.");
+                }
+
+                var failedCleanup = false;
+                try
+                {
+                    await cluster.QueryIndexes.BuildDeferredIndexesAsync(bucketName, options =>
+                    {
+                        options.ScopeNameValue = scopeName;
+                        options.CollectionNameValue = collectionName;
+                    }).ConfigureAwait(false);
+
+                    using var cts = new CancellationTokenSource(10000);
+
+                    await cluster.QueryIndexes.WatchIndexesAsync(bucketName, new[] {indexName},
+                        options =>
+                        {
+                            options.CancellationToken(cts.Token);
+                            options.ScopeNameValue = scopeName;
+                            options.CollectionNameValue = collectionName;
+                        }).ConfigureAwait(false);
+
+                    var getIndexes = await cluster.QueryIndexes.GetAllIndexesAsync(bucketName, options =>
+                    {
+                        options.ScopeNameValue = scopeName;
+                        options.CollectionNameValue = collectionName;
+                    });
+                    Assert.Contains(indexName, getIndexes.Select(idx => idx.Name));
+                }
+                finally
+                {
+                    try
+                    {
+                        await cluster.QueryIndexes.DropIndexAsync(bucketName, indexName, options =>
+                        {
+                            options.ScopeNameValue = scopeName;
+                            options.CollectionNameValue = collectionName;
+                        }).ConfigureAwait(false);
+                    }
+                    catch (Exception e)
+                    {
+                        _outputHelper.WriteLine($"Failure during cleanup: {e}");
+                        failedCleanup = true;
+                    }
+                }
+
+                Assert.False(failedCleanup);
+            }
+            finally
+            {
+                await collectionManager.DropScopeAsync(scopeName);
+            }
+
         }
     }
 }
