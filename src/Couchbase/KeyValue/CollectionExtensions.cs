54fde6487d9aa5b31f50269350ba09a18a3fd9bf
Emilien Bevierre <emilien.bevierre@couchbase.com>
2026-09-17 12:27:28 +0100

    NCBC-4294: Fix attribute indentation

--- src/Couchbase/KeyValue/CollectionExtensions.cs
+++ src/Couchbase/KeyValue/CollectionExtensions.cs
@@ -455,7 +455,7 @@ namespace Couchbase.KeyValue
         /// <param name="id">The id of the document.</param>
         /// <param name="strategy">Chooses which replica to read from.</param>
         /// <returns>An asynchronous <see cref="Task"/> containing an <see cref="IGetReplicaResult"/>.</returns>
-[InterfaceStability(Level.Uncommitted)]
+        [InterfaceStability(Level.Uncommitted)]
         public static Task<IGetReplicaResult> GetReplicaAsync(this ICouchbaseCollection collection, string id, GetReplicaStrategy strategy)
         {
             return collection.GetReplicaAsync(id, strategy, GetReplicaOptions.Default);
@@ -469,7 +469,7 @@ namespace Couchbase.KeyValue
         /// <param name="strategy">Chooses which replica to read from.</param>
         /// <param name="configureOptions">Optional parameters</param>
         /// <returns>An asynchronous <see cref="Task"/> containing an <see cref="IGetReplicaResult"/>.</returns>
-[InterfaceStability(Level.Uncommitted)]
+        [InterfaceStability(Level.Uncommitted)]
         public static Task<IGetReplicaResult> GetReplicaAsync(this ICouchbaseCollection collection, string id, GetReplicaStrategy strategy, Action<GetReplicaOptions> configureOptions)
         {
             var options = new GetReplicaOptions();
